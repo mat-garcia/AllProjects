6b27f88623941efa1417415024c5da81eb6e3b49
mat-garcia <mateussoap@gmail.com>
2022-06-14 13:39:06 -0300

    incio projeto

---
+++ orgs-cesta/App.js
@@ -0,0 +1,15 @@
+
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import Cesta from './src/telas/cesta';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Cesta/>
+      
+    </SafeAreaView>
+  );
+}
+
